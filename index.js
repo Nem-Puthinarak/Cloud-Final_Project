d77ae57723e44b3a475902257ce62e8a4eb0be1e
Narak <nrraiyugin@gmail.co>
2026-04-27 09:41:33 +0700

    add index

---
+++ index.js
@@ -0,0 +1,14 @@
+// IMPORT Express Server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1>i think it's workingg nowww...</h1>")
+})
+
+app.post('/', (req, res)=> {
+    res.send("Received!")
+})
+
+app.listen(5000, () =>
+console.log('EXPRESS Server Started at Port No: 5000'));
